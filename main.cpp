77c4a5531562e2d2b4b019f581ff3f9fc1803a95
Khoa Trần <khoatranprogrammer@gmail.com>
2019-10-12 02:26:18 +0700

    [000002] Get black/white and labeling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QCoreApplication>
 #include <QFile>
 #include <QByteArray>
+#include <QDir>
+#include <QDebug>
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
@@ -9,20 +11,101 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char *argv[])
+Mat ReadImageFromNonAsciiName(QString path)
 {
-    QString path = "E:/TuDienHOA/01_DuLieu/CameraCapture/02_Data/cha_jpg_nonascii/䳗.jpg";
     QFile file(path);
     std::vector<char> buffer;
     buffer.resize(file.size());
-    if (!file.open(QIODevice::ReadOnly)) {
-        return 1;
-    }
+
+    file.open(QIODevice::ReadOnly);
     file.read(buffer.data(), file.size());
     file.close();
-    cv::Mat image = cv::imdecode(buffer, cv::IMREAD_UNCHANGED);
-    imshow("Display Image", image);
-    cv::waitKey(0);
+
+    Mat image = imdecode(buffer, cv::IMREAD_UNCHANGED);
+
+    return image;
+}
+
+void CountBlackWhite(Mat bw, uint* black_pixels, uint* white_pixels)
+{
+    uint black = 0;
+    uint white = 0;
+    for (int i = 0; i < bw.cols; i++) {
+        for (int j = 0; j < bw.rows; j++) {
+            uchar pixelGrayValue = bw.at<uchar>(j, i);
+            if (pixelGrayValue) white++;
+            else black++;
+        }
+    }
+
+    *black_pixels = black;
+    *white_pixels = white;
+}
+
+int ImageLabeling(Mat image)
+{
+    int result = 0;
+
+    Mat labels;
+    Mat stats;
+    Mat centroids;
+    cv::connectedComponentsWithStats(image, labels, stats, centroids);
+
+    for (int i = 0; i < stats.rows; i++)
+    {
+        int x = stats.at<int>(Point(0, i));
+        int y = stats.at<int>(Point(1, i));
+        int w = stats.at<int>(Point(2, i));
+        int h = stats.at<int>(Point(3, i));
+
+        if((w != image.cols) && (h != image.rows))
+        {
+            result++;
+//            std::cout << "x=" << x << " y=" << y << " w=" << w << " h=" << h << std::endl;
+//            Scalar color(255, 0, 0);
+//            Rect rect(x, y, w, h);
+//            cv::rectangle(image, rect, color);
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    uint numofimages = 1;
+    QDir directory("E:/TuDienHOA/01_DuLieu/CameraCapture/02_Data/cha_jpg_nonascii/");
+    QStringList images = directory.entryList(QStringList() << "*.jpg" << "*.JPG",QDir::Files);
+    foreach(QString filename, images) {
+        QString path = "E:/TuDienHOA/01_DuLieu/CameraCapture/02_Data/cha_jpg_nonascii/" + filename;
+        Mat image = ReadImageFromNonAsciiName(path);
+
+        // Convert image to grayscale
+        Mat gray;
+        cvtColor(image, gray, COLOR_BGR2GRAY);
+
+        // Convert image to binary
+        Mat bw;
+        threshold(gray, bw, 50, 255, THRESH_BINARY | THRESH_OTSU);imshow("Display Image", bw);
+        //! [pre-process]
+
+        // Black and white counting
+        //! [black and white]
+        uint black_pixels = 0;
+        uint white_pixels = 0;
+
+        CountBlackWhite(bw, &black_pixels, &white_pixels);
+
+        // Image labeling
+        uint numoflabel = ImageLabeling(bw);
+
+        cout
+            << "Index: " << numofimages++
+            << " - Number of label: " << numoflabel
+            << " - Black pixels: " << black_pixels
+            << " - White pixels: " << white_pixels
+            << std::endl;
+    }
 
     return 0;
 }
